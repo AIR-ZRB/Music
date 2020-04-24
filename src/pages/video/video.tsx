a306906eedd38fc2df8f2c75e5fcad3184202d2b
青空 <1057605824@qq.com>
2020-04-24 18:56:44 +0800

    模块完成video

--- src/pages/video/video.tsx
+++ src/pages/video/video.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./video.scss"
 
 export default class video extends React.Component<any,any> {
     constructor(props: any){
@@ -9,7 +9,24 @@ export default class video extends React.Component<any,any> {
 
     render(){
         return (
-            <div>video</div>
+            <div className="video">
+
+
+                <section className="videoList">
+                    <img src="https://pic1.zhimg.com/80/v2-cc4f66fc089be551f5509d063c96deec_720w.jpg" alt=""/>
+                    <div className="video-describe">
+                        <p>樱桃小丸子</p>    
+                        <p>by 咕咕咕咕</p>
+                    </div>
+                </section>
+
+            </div>
         )
     }
 }
+
+
+
+// function videoList(props: any){
+//     return 
+// }
